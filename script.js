03351367662e17ceefdf5751dc581aaa0adf7a8a
OlgaIsayeva <139140640+OlgaIsayeva@users.noreply.github.com>
2023-07-11 12:58:37 +0200

    Updated scripts.js

--- script.js
+++ script.js
@@ -1,1 +1,48 @@
-alert("");
+function formatDate() {
+  let now = new Date();
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+  let day = days[now.getDay()];
+  let hours = now.getHours();
+  let minutes = now.getMinutes();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  let currentTime = document.querySelector("#time");
+  currentTime.innerHTML = `${day} ${hours}:${minutes}`;
+}
+formatDate();
+
+function cityWeather(response) {
+  document.querySelector("#city").innerHTML = response.data.name;
+  document.querySelector("#temperature").innerHTML = Math.round(
+    response.data.main.temp
+  );
+}
+
+function city(event) {
+  event.preventDefault();
+  let apiKey = "6d68aadfacdd4f5163bc273049a0cf2d";
+  let city = document.querySelector("#city-input").value;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(cityWeather);
+}
+let searchForm = document.querySelector(".search-form");
+searchForm.addEventListener("submit", city);
+
+function searchLocation(position) {
+  let apiKey = "6d68aadfacdd4f5163bc273049a0cf2d";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=metric`;
+
+  axios.get(apiUrl).then(cityWeather);
+}
+
+function getCurrentLocation(event) {
+  event.preventDefault();
+  navigator.geolocation.getCurrentPosition(searchLocation);
+}
+
+let currentLocationButton = document.querySelector("#current-location-button");
+currentLocationButton.addEventListener("click", getCurrentLocation);
